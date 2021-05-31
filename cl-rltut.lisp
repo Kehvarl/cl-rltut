56650f3ccbea283e818369871ea28f5324813c3d
Kehvarl <kehvarl@kehvarl.com>
2021-05-31 11:25:23 -0700

    draw a window

--- cl-rltut.lisp
+++ cl-rltut.lisp
@@ -1,3 +1,23 @@
 ;;;; cl-rltut.lisp
 
 (in-package #:cl-rltut)
+(defparameter *screen-width* 80)
+(defparameter *screen-height* 50)
+
+(defun draw ()
+  (blt:clear)
+  (blt:refresh))
+
+(defun config ()
+  (blt:set "window.resizeable = true")
+  (blt:set "window.size = ~Ax~A" *screen-width* *screen-height*)
+  (blt:set "window.title = Roguelike"))
+
+(defun main()
+  (blt:with-terminal
+    (config)
+    (loop :do
+      (draw)
+      (blt:key-case (blt:read)
+		    (:escape (return))
+		    (:close (return))))))
