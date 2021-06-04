20c1f564bd8916e6b891e3b2e9f7be6006cbd952
Kehvarl <kehvarl@kehvarl.com>
2021-06-04 16:52:12 -0700

    create gamemap class

--- game-map.lisp
+++ game-map.lisp
@@ -13,3 +13,13 @@
   (with-slots (blocked block-sight) tile
     (if (null block-sight)
 	(setf block-sight blocked))))
+
+(defclass game-map ()
+  ((width :initarg :w :accessor game-map/w)
+   (height :initarg :h :accessor game-map/h)
+   (times :accessor game-map/tiles)))
+
+(defmethod initialize-instance :after ((map game-map) &rest initargs)
+  (declare (ignore initargs))
+  (setf (game-map/tiles map) (make-array (list (game-map/w map) (game-map/h map)))))
+
