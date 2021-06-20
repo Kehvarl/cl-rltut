f2a83cb82050b8d39738c72a8943968808e27935
Kehvarl <kehvarl@kehvarl.com>
2021-06-19 22:51:30 -0700

    create fighter component

---
+++ components.lisp
@@ -0,0 +1,10 @@
+(in-package #:cl-rltut)
+
+(defclass component ()
+  ((owner :initarg :owner :accessor component/owner)))
+
+(defclass fighter (component)
+  ((max-hp :initarg :max-hp :accessor fighter/max-hp :initform nil)
+   (hp :initarg :hp :accessor fighter/hp)
+   (defense :initarg :defense :accessor fighter/defense)
+   (attack :initarg :attack :accessor fighter/attack)))
