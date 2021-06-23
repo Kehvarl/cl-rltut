ea98956a440a6031cdbfb7395edad7ad2c80fd98
Kehvarl <kehvarl@kehvarl.com>
2021-06-22 18:26:27 -0700

    basic monster component.   turn-taking concept

--- components.lisp
+++ components.lisp
@@ -8,3 +8,11 @@
    (hp :initarg :hp :accessor fighter/hp)
    (defense :initarg :defense :accessor fighter/defense)
    (attack :initarg :attack :accessor fighter/attack)))
+
+
+(defclass basic-monster (component) ())
+
+(defgeneric take-turn (component))
+
+(defmethod take-turn ((component basic-monster))
+  (format t "The ~A wonders when it will get to move." (component/owner component)))
